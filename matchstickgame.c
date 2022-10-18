4ce5f38661df6752bbecf9dc506587446b8a0ad2
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2022-10-18 20:59:51 +0530

    Add files via upload

---
+++ matchstickgame.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int total,a,b,i,rem=21;
+    printf("                                             RULE: One can pick only upto 4 Matchstick at a time.\n\n");
+    for(i=0;i<=rem;i++)
+    {
+        printf("YOUR TURN :");
+        scanf("%d",&a);
+        if(a>4)
+        {
+        printf("\n\n!Follow the rule! \n\n YOU LOST");
+        exit(0);
+        }
+        printf("I will pick %d Matchstick\n",(5-a));
+        b=5-a;
+        rem-=(a+b);
+        printf("Remaining:%d\n\n\n",rem);
+        if(rem==1)
+        printf("YOU LOST!");
+    }
+}
