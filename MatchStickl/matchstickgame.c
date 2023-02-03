ccc4ebd624532216a4f06181180b6ec521f052ef
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2023-02-03 18:24:38 +0000

    Re arranged

---
+++ MatchStickl/matchstickgame.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+int m = 21, p, c;
+while (1) {
+    printf("\n\t\tRules: you pick 1 or 2 or 3 or 4 matchsticks at a time!!!\n\n\n\n");
+    printf("\nNumber of Match sticks left = %d\n", m);
+    printf("Pick 1 or 2 or 3 or 4 matches\n");
+    scanf("%d", &p);
+
+    if (p > 4 || p < 1){
+        printf("follow the rukes\n");
+        continue;
+    }
+    m = m - p;
+
+    printf("Number of matches left = %d\n", m);
+
+    c = 5 - p;
+
+    printf("out of which computer picked up %d\n", c);
+
+    m = m - c;
+
+    if (m == 1) {
+      printf("\nNumber of matches left = %d\n", m);
+      printf("You lost the Game\n");
+      break;
+    }
+  }
+
+  return 0;
+}
